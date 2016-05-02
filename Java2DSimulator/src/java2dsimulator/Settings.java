596fd5d77a7e41b143f5e875dfc1a6a43d20c96a
JuNe98 <julian111@gmx.at>
2016-05-02 16:46:33 +0200

    Merge origin/master
    Conflicts:
    	NOR-Player/settings.norc

--- Java2DSimulator/src/java2dsimulator/Settings.java
+++ Java2DSimulator/src/java2dsimulator/Settings.java
@@ -1,20 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package java2dsimulator;
 
 /**
  *
- * @author stirmayr
+ * @author Matthias Stirmayr
  */
 public class Settings {
 
     public static double SCENE_WIDTH = 1920;
     public static double SCENE_HEIGHT = 1000;
 
-/**
+    /**
      * The scale 64 pixels per meter - where 1m = 1 sprite width
      */
     public static final double SCALE = 64;
