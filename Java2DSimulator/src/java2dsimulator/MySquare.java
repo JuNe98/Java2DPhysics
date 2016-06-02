448a5d4f3604864ed9df29719086199b2d2e5151
JuNe98 <julian111@gmx.at>
2016-06-02 10:23:08 +0200

    Merge origin/master
    Conflicts:
    	NOR-Player/settings.norc

--- Java2DSimulator/src/java2dsimulator/MySquare.java
+++ Java2DSimulator/src/java2dsimulator/MySquare.java
@@ -11,7 +11,7 @@ import org.dyn4j.geometry.Circle;
 public class MySquare implements Object2D{
     private int length;
     private ArrayList<Particle> particles;
-    private Point mouseposition;
+    private Point mouseposition = new Point();
 
     public MySquare(int length) {
         this.length = length;
@@ -46,31 +46,54 @@ public class MySquare implements Object2D{
                 int x = 0;
                 int y = 0;
                 if(length%2==0){
-                    if(i<length/2){
-                        y = ((int)mouseposition.getY())-(length/2)*8-(length/2-1)*1-4;
-                    }else if(i>length/2){
-                        y = ((int)mouseposition.getY())+(length/2)*8+(length/2-1)*1-4;
+                    if(i+1<length/2){
+                        //y = (int) (Settings.SCENE_HEIGHT-(((int)mouseposition.getY())-(length/2)*8-(length/2-1)*1-4));
+                        y = (int) mouseposition.getY()+(length/2)*8+(length/2-1)+4;
+                    }else if(i+1>length/2){
+                        //y = (int) (Settings.SCENE_HEIGHT-(((int)mouseposition.getY())+(length/2)*8+(length/2-1)*1-4));
+                        y = (int) mouseposition.getY()-(length/2)*8-(length/2-1)+4;
                     }
-                    if(j<length/2){
-                        x = ((int)mouseposition.getX())-(length/2)*8-(length/2-1)*1-4;
-                    }else if(j>length/2){
-                        x = ((int)mouseposition.getX())+(length/2)*8+(length/2-1)*1-4;
+                    if(j+1<length/2){
+                        //x = ((int)mouseposition.getX())-(length/2)*8-(length/2-1)*1-4;
+                        x = (int) mouseposition.getX() - (length/2)*8 - (length/2-1)*1 - 4;
+                    }else if(j+1>length/2){
+                        //x = ((int)mouseposition.getX())+(length/2)*8+(length/2-1)*1-4;
+                        x = (int) mouseposition.getX() + (length/2)*8 + (length/2-1)*1 - 4;
                     }
+                    
                 } else{
-                    if(i<length/2){
-                        y = ((int)mouseposition.getY())-4-((length-1)/2)*8-((length-1)/2)*1-4;
-                    }else if(i>length/2){
-                        y = ((int)mouseposition.getY())+4+((length-1)/2)*8+((length-1)/2)*1-4;
+                    if(i+1<length/2){
+                        //y = (int) (Settings.SCENE_HEIGHT-(((int)mouseposition.getY())-(length/2)*8-(length/2-1)*1-4));
+                        y = (int) mouseposition.getY()+(length/2-i)*8+(length/2-i)+4;
+                    }else if(i+1>length/2){
+                        //y = (int) (Settings.SCENE_HEIGHT-(((int)mouseposition.getY())+(length/2)*8+(length/2-1)*1-4));
+                        y = (int) mouseposition.getY()-((length/2-(i-length/2))-1)*8-(length/2-(i-length/2))+4;
+                        if(i+1 == length){
+                            y -= 1;
+                        }
                     }else{
-                        y = (int)mouseposition.getY()-4;
+                        //y = (int)(Settings.SCENE_HEIGHT-((int)mouseposition.getY()-4));
+                        y = (int) mouseposition.getY()+4;
+                    }
+                    if(j+1<length/2){
+                        //x = ((int)mouseposition.getX())-(length/2)*8-(length/2-1)*1-4;
+                        x = (int) mouseposition.getX() - (length/2-j)*8 - (length/2-j) - 4;
+                        if(j == 0){
+                            x += 1;
+                        }
+                    }else if(j+1>length/2){
+                        //x = ((int)mouseposition.getX())+(length/2)*8+(length/2-1)*1-4;
+                        x = (int) mouseposition.getX() + ((length/2-(j-length/2))-1)*8 + (length/2-(j-length/2)) - 4;
+                    }else{
+                        x = (int) mouseposition.getX()-4;
                     }
                 }
-                double dy = y/64;
+                double dy = (Settings.SCENE_HEIGHT-y)/Settings.SCALE;
                 double dx = 0;
                 if(x<Settings.SCENE_WIDTH/2){
-                    //dx = ;
+                    dx = -(((Settings.SCENE_WIDTH/2)-x)/Settings.SCALE);
                 }else if(x>Settings.SCENE_WIDTH/2){
-                    dx = (x/64)-(Settings.SCENE_WIDTH/2)/64;
+                    dx = (x-(Settings.SCENE_WIDTH/2))/Settings.SCALE;
                 }
                 Particle particle = new Particle(dx,dy);
                 particles.add(particle);
